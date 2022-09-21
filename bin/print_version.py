4b7f73e434a4920a1d72f55ec9e6ef1e7871935d
George Babarus <george.babarus@gmail.com>
2022-09-21 14:56:52 +0300

    update dependencies

--- bin/print_version.py
+++ bin/print_version.py
@@ -1,8 +1,7 @@
 import sys
 import os
 
-
-sys.path.append(os.path.abspath("./../src"))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 
 from lcli import __version__
 
