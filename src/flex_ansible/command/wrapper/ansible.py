adef41b1c7d3f0b949322a57520f3d27db1a231d
George Babarus <george.babarus@gmail.com>
2022-09-22 15:34:50 +0300

    ansible wrapper for flex-cli

---
+++ src/flex_ansible/command/wrapper/ansible.py
@@ -0,0 +1,61 @@
+import ansible.cli
+from ansible.playbook import Playbook
+from ansible.cli.playbook import PlaybookCLI
+from lcli.app import App
+from lcli.command.builders import BaseBuilder
+from lcli.command.wrappers import BaseCommandWrapperInterface, BashCommandWrapper
+from lcli.config import Command, CommandCollection
+import sys
+import os
+
+
+class AnsibleWrapper(BashCommandWrapper, BaseCommandWrapperInterface):
+    """
+    Wrapp ansible command with autocomplete and suggestions functionality
+    """
+
+    _command_name: str
+    _command: Command
+
+    def __init__(self, app: App, command: Command) -> None:
+        command = self.__init_playbook_methods(app, command)
+        super().__init__(app, command)
+
+    def __init_playbook_methods(self, app: App, command: Command):
+        # arguments = sys.argv
+        arguments = ['ansible-playbook']
+
+        playbook_path = os.path.abspath(command.args.get('playbook'))
+        arguments.append(playbook_path)
+        arguments.append('-i')
+        arguments.append(command.args.get('inventory'))
+
+        cli = PlaybookCLI(arguments)
+        cli.parse()
+        loader, inventory, variable_manager = cli._play_prereqs()
+        pb = Playbook.load(playbook_path, variable_manager=variable_manager, loader=loader)
+        plays = pb.get_plays()
+        play: ansible.playbook.play.Play
+        command_dict = {}
+        for play in plays:
+            command_dict[play.vars['lcli']['id']] = {
+                'description': play.vars['lcli']['description'],
+                'name': play.vars['lcli']['id'],
+                'type': 'cli',
+                'args': {
+                    'command': ' '.join(['ansible-playbook', playbook_path, '-i', command.args.get('inventory'), '--tags=security,performance,db_servers'])
+                }
+            }
+
+        command = command.get()
+        command['commands'] = CommandCollection(command_dict)
+        return Command(command['name'], command)
+
+    class _Builder(BaseBuilder):
+        """
+        This will be a custom builder for current wrapper, custom ones may be implemented
+        """
+        command_type: str = "ansible_wrapper"
+
+        def build(self, command: Command) -> 'AnsibleWrapper':
+            return AnsibleWrapper(self._app, command)
--- src/lcli/command/__init__.py
+++ src/lcli/command/__init__.py
@@ -4,8 +4,10 @@ from lcli.command.builders import (
     SimpleBuilder,
     WrappersBuilder,
 )
-
+from flex_ansible.command.wrapper.ansible import AnsibleWrapper
 
 CommandBuilderFactory.register_builder(LcliBuilder)
 CommandBuilderFactory.register_builder(SimpleBuilder)
 CommandBuilderFactory.register_builder(WrappersBuilder)
+CommandBuilderFactory.register_builder(WrappersBuilder)
+CommandBuilderFactory.register_builder(AnsibleWrapper)
